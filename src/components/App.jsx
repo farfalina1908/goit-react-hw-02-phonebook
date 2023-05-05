cdb6ed6b1a86f2eda5ac0e835afb7c0e0aa8d2eb
Elena Berezina <farfalina1908@gmail.com>
2023-05-06 00:38:52 +0300

    update css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,14 @@ import css from './Phonebook.module.css';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
     >
       <Phonebook />
     </div>
