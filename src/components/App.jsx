cc423a771aa67a066a7da43598285b5f92504226
Elena Berezina <farfalina1908@gmail.com>
2023-05-06 20:28:38 +0300

    update styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,7 @@ import css from './Phonebook.module.css';
 
 export const App = () => {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101',
-      // }}
-    >
+    <div>
       <Phonebook />
     </div>
   );
